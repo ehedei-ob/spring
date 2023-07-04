d268d079db87161952a0031a9ab3081b0214e09c
Ehedei <ehedeidev@gmail.com>
2023-07-04 17:48:41 +0200

    First commit

---
+++ src/main/java/dev/ehedei/exerciseob/ExerciseObApplication.java
@@ -0,0 +1,13 @@
+package dev.ehedei.exerciseob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseObApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseObApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ehedei/exerciseob/ExerciseObApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ehedei.exerciseob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseObApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
